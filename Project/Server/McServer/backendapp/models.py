17cdc546c029c76dc088590f653d7f305f9150a9
Tarun Chinmai <tchinmai7@gmail.com>
2019-04-29 15:29:56 -0700

    Add django framework for serveR

---
+++ Project/Server/McServer/backendapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    UserName = models.CharField(max_length=100)
+    ClassifierName = models.CharField(max_length=100)
+    UserSignalFile = models.FileField(blank=False, null=False)
+
+    def __str__(self):
+        return "{0}, {1}, {2}".format(self.ClassifierName, self.UserName, self.UserSignalFile.name)
